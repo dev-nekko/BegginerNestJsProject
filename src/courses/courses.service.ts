86e20fefee30679c990a54b581a0981da7bea6b5
xfrogz <renattoyague@gmail.com>
2022-05-15 03:48:57 -0500

    Add Get Courses service

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,7 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { COURSES } from './courses.mock';
 
 @Injectable()
 export class CoursesService {
     courses = COURSES;
+    
+    getCourses(): Promise<any> {
+        return new Promise(resolve => {
+            resolve(this.courses);
+        });
+    }
+
+    getCourse(courseId): Promise<any> {
+        let id = Number(courseId);
+        return new Promise(resolve => {
+            const course = this.courses.find(course => course.id === id);
+            if (!course) {
+                throw new HttpException('Course does not exist', 404)
+            }
+            resolve(course);
+        });
+    }
 }
