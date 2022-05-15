d1cd047bbd4dd0e01941e1bfe5b50131aeeba230
xfrogz <renattoyague@gmail.com>
2022-05-15 03:56:55 -0500

    Implementation in the controller of RESTful service

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get, Param, Post, Body, Delete, Query } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+    constructor(private coursesService: CoursesService) {}
+
+    @Get()
+    async getCourses() {
+        const courses = await this.coursesService.getCourses();
+        return courses;
+    }
+
+    @Get(':courseId')
+    async getCourse(@Param('courseId') courseId) {
+        const course = await this.coursesService.getCourse(courseId);
+        return course;
+    }
+
+    @Delete()
+    async deleteCourse(@Query() query) {
+        const courses = await this.coursesService.deleteCourse(query.courseId);
+        return courses;
+    }
+}
